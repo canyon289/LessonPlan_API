90af1ac69c1c8a094d58bf19a78b30f0d42e436f
canyon <ravinsdrive@gmail.com>
2018-08-20 16:40:34 -0500

    Add Unsolved file

---
+++ 04-Stu_Get_Stock_Ticker/Unsolved/GetStockTicker_Unsolved.py
@@ -0,0 +1,19 @@
+# Dependencies
+import quandl
+import json
+
+# API Key
+# YOUR CODE HERE
+raise NotImplementedError
+
+# Setup Quandl API Authentication
+# YOUR CODE HERE
+raise NotImplementedError
+
+# Get stock ticker for Apple
+# YOUR CODE HERE
+raise NotImplementedError
+
+# Loop through prices and dates and print each
+# YOUR CODE HERE
+raise NotImplementedError
